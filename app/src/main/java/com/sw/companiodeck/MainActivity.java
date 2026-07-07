9c954e030dc014df77b8345dcb1e5db6c4b64fdb
VouizS <scared7j@gmail.com>
2026-07-07 00:36:46 -0300

    Companion Deck v1.1-r2: gb gbc core integration

--- app/src/main/java/com/sw/companiodeck/MainActivity.java
+++ app/src/main/java/com/sw/companiodeck/MainActivity.java
@@ -36,7 +36,7 @@ import java.util.Locale;
 
 public class MainActivity extends Activity {
     private static final int REQUEST_PICK_ROM = 1201;
-    private static final String INTERNAL_VERSION = "v1.1";
+    private static final String INTERNAL_VERSION = "v1.1-r2";
     private static final String PREFS = "companion_deck_native_prefs";
     private static final String KEY_GAMES = "games_native_v1";
     private static final String[] PLATFORM_IDS = {"gbc", "gba", "snes", "psp", "ps1", "n64", "cubewii", "ps2", "manual"};
