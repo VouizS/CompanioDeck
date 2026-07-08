906eb405689c5c817b390c811fe2a926c920fe09
VouizS <scared7j@gmail.com>
2026-07-08 15:49:02 -0300

    Companion Deck v1.1-r16: clean workflow rebuild

--- app/src/main/java/com/sw/companiodeck/MainActivity.java
+++ app/src/main/java/com/sw/companiodeck/MainActivity.java
@@ -39,7 +39,7 @@ import java.util.zip.ZipInputStream;
 
 public class MainActivity extends Activity {
     private static final int REQUEST_PICK_ROM = 1201;
-    private static final String INTERNAL_VERSION = "v1.1-r15";
+    private static final String INTERNAL_VERSION = "v1.1-r16";
     private static final String PREFS = "companion_deck_native_prefs";
     private static final String KEY_GAMES = "games_native_v1";
     private static final String[] PLATFORM_IDS = {"gbc", "gba", "snes", "psp", "ps1", "n64", "cubewii", "ps2", "manual"};
